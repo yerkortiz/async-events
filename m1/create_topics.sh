c93e2e9cb59349145a9018dfb8c4c6bf6f45d4ec
yerkortiz <yerko.ortizmora@gmail.com>
2021-08-12 22:31:21 -0400

    fix: error in kafka scripts

--- m1/create_topics.sh
+++ m1/create_topics.sh
@@ -2,12 +2,37 @@
 
 source kafka_home.sh
 
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --config retention.ms=259200000 --topic order_received
+$KAFKA_HOME/bin/kafka-topics.sh --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --config retention.ms=259200000 \
+    --topic order_received
 
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --config retention.ms=259200000 --topic order_confirmed
+$KAFKA_HOME/bin/kafka-topics.sh --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --config retention.ms=259200000 \
+    --topic order_confirmed
 
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --config retention.ms=259200000 --topic order_ready
+$KAFKA_HOME/bin/kafka-topics.sh --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --config retention.ms=259200000 \
+    --topic order_ready
 
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --config retention.ms=259200000 --topic order_notification
+$KAFKA_HOME/bin/kafka-topics.sh --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --config retention.ms=259200000 \
+    --topic order_notification
 
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --config retention.ms=259200000 --topic order_error
+$KAFKA_HOME/bin/kafka-topics.sh --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --config retention.ms=259200000 \
+    --topic order_error
--- m1/kafka_home.sh
+++ m1/kafka_home.sh
@@ -1,1 +1,1 @@
-KAFKA_HOME='~/codes/kafka/kafka_2.13-2.8.0'
+KAFKA_HOME=~/codes/kafka/kafka_2.13-2.8.0
