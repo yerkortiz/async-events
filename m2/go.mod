061d2ac54cd7b7be153b335caaf9b37773a5dd10
yerkortiz <yerko.ortizmora@gmail.com>
2021-09-01 16:29:58 -0400

    add: publish event

--- m2/go.mod
+++ m2/go.mod
@@ -2,4 +2,7 @@ module milestone2
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/segmentio/kafka-go v0.4.17
+)
--- m2/main.go
+++ m2/main.go
@@ -1,9 +1,42 @@
 package main
 
-import "milestone2/server"
+import (
+	"log"
+	"milestone2/events"
+	"milestone2/server"
+	"os"
+	"time"
+)
 
 func init() {
-
+	log.SetOutput(os.Stdout)
+	orderReceived := events.OrderReceived{
+		EventID:    12312,
+		EventState: "Received",
+		Timestamp:  time.Now(),
+		EventMessage: events.Order{
+			OrderID: 1231,
+			Total:   1000,
+			Customer: events.Customer{
+				Email:    "asdas@mail.com",
+				FistName: "caca",
+				LastName: "seca",
+				ShippingAddress: events.ShippingAddress{
+					StreetAddress: "casa",
+					City:          "ciudad",
+					Commune:       "comuna",
+					Country:       "pais",
+					ZipCode:       "12312312",
+				},
+			},
+			Products: []events.Product{{
+				ProductID:   123132,
+				Price:       1000,
+				ProductName: "inverse suffix tree",
+			}},
+		},
+	}
+	events.PublishEvent(orderReceived, 0)
 }
 func main() {
 	server.RunServer()
