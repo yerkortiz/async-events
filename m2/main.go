4f4f3d9bd60cbbfbde7724295cd4375d969c2a7e
yerkortiz <yerko.ortizmora@gmail.com>
2021-09-01 00:41:26 -0400

    add: mux

--- m2/main.go
+++ m2/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+	http.ListenAndServe(":8080", r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
